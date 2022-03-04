f0a82b219fa02ec55e77c5ebd8b5930cb9b13565
Luis Eduardo Pozos Miranda <82684590+PozosLuis@users.noreply.github.com>
2022-03-04 15:52:13 -0600

    Add files via upload

--- Estructura/Pastelero/Ver_Pedido.php
+++ Estructura/Pastelero/Ver_Pedido.php
@@ -18,5 +18,37 @@
         <td></td>
     </tr>
 </table>
+<br>
+<br>
+<h3>Sabores</h3>
+<table>
+    <tr>
+        <th>No</th>
+        <th>Sabores</th>
+        <th>Cantidad</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<br>
+<br>
+<h3>Adornos</h3>
+<table>
+    <tr>
+        <th>No</th>
+        <th>Adornos</th>
+        <th>Cantidad</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<br>
+<br>
 <form action=""><input type="button" value="Atras" onclick = "history.back()"></form>
 <?php include('Pie.php')?>
